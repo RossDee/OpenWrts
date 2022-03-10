c5c15d3e6c426462e2fccd3b8e13390207e99dbe
RossDee <60028714+RossDee@users.noreply.github.com>
2022-03-10 12:31:04 +0800

    Update package.sh
    add neobird theme

--- package.sh
+++ package.sh
@@ -3,3 +3,4 @@ git clone  https://github.com/bigbugcc/OpenwrtApp package/OpenwrtApp
 git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone  https://github.com/RossDee/luci-app-passwall package/passwall
 git clone  https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+git clone https://github.com/thinktip/luci-theme-neobird.git  
